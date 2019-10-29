1b83ceb9e9883084437d7ab9c718af35df823d91
Luis F M Alves <luis.projensino@gmail.com>
2019-10-28 21:54:04 -0300

    first commit

---
+++ lib/telegrambot.ex
@@ -0,0 +1,18 @@
+defmodule Telegrambot do
+  @moduledoc """
+  Documentation for Telegrambot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Telegrambot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
